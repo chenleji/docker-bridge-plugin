692f9e289957e239c3eda4e47f6941f23b7f864f
Chen Leji <chenleji@ChenLeji-MacBook-Pro.local>
2016-07-25 13:31:49 +0800

    add code.

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+	"github.com/gopher-net/dknet"
+	"github.com/chenleji/docker-bridge-plugin/ovs"
+)
+
+const (
+	version = "0.2"
+)
+
+func main() {
+
+	var flagDebug = cli.BoolFlag{
+		Name:  "debug, d",
+		Usage: "enable debugging",
+	}
+	app := cli.NewApp()
+	app.Name = "don"
+	app.Usage = "Docker Open vSwitch Networking"
+	app.Version = version
+	app.Flags = []cli.Flag{
+		flagDebug,
+	}
+	app.Action = Run
+	app.Run(os.Args)
+}
+
+// Run initializes the driver
+func Run(ctx *cli.Context) {
+	if ctx.Bool("debug") {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	d, err := ovs.NewDriver()
+	if err != nil {
+		panic(err)
+	}
+	h := dknet.NewHandler(d)
+	h.ServeUnix("root", "ovs")
+}
